bdf2652db975cf30b864d9b70e94c818a3f2017a
jaowie <94178771+jaowie@users.noreply.github.com>
2023-01-30 21:22:55 +0800

    waw

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("spinner-container").style.display = "flex";
+    setTimeout(function() {
+      document.getElementById("spinner-container").style.display = "none";
+    }, 2200);
+  });
+
+  
